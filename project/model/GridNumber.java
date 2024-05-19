f08bbe3d8ffb221a3a0bf7353e797554b843b9c4
张岩 <12313327@mail.sustech.edu.cn>
2024-05-19 21:41:05 +0800

    5.19

--- project/model/GridNumber.java
+++ project/model/GridNumber.java
@@ -21,17 +21,72 @@ public class GridNumber {
     public void initialNumbers() {
         for (int i = 0; i < numbers.length; i++) {
             for (int j = 0; j < numbers[i].length; j++) {
-                //todo: update generate numbers method
-                numbers[i][j] = random.nextInt(2) == 0 ? 1 : 0;
+                numbers[i][j] = 0;
             }
         }
+        // Place initial tiles
+        placeRandomTile();
+        placeRandomTile();
     }
-    //todo: finish the method of four direction moving.
-    public void moveRight() {
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i][1] += numbers[i][0];
-            numbers[i][0] = 0;
+
+    private void placeRandomTile() {
+        int x, y;
+        do {
+            x = random.nextInt(X_COUNT);
+            y = random.nextInt(Y_COUNT);
+        } while (numbers[x][y] != 0);
+        numbers[x][y] = random.nextInt(2) == 0 ? 2 : 4;
+    }
+
+    private void move(int dx, int dy) {
+        boolean moved = false;
+
+        for (int i = 0; i < X_COUNT; i++) {
+            for (int j = 0; j < Y_COUNT; j++) {
+                int x = dx > 0 ? X_COUNT - 1 - i : i;
+                int y = dy > 0 ? Y_COUNT - 1 - j : j;
+                if (numbers[x][y] != 0) {
+                    int nx = x, ny = y;
+                    while (true) {
+                        int nextX = nx + dx, nextY = ny + dy;
+                        if (nextX < 0 || nextX >= X_COUNT || nextY < 0 || nextY >= Y_COUNT || (numbers[nextX][nextY] != 0 && numbers[nextX][nextY] != numbers[nx][ny])) {
+                            break;
+                        }
+                        nx = nextX;
+                        ny = nextY;
+                    }
+                    if (nx != x || ny != y) {
+                        if (numbers[nx][ny] == 0) {
+                            numbers[nx][ny] = numbers[x][y];
+                        } else if (numbers[nx][ny] == numbers[x][y]) {
+                            numbers[nx][ny] *= 2;
+                        }
+                        numbers[x][y] = 0;
+                        moved = true;
+                    }
+                }
+            }
         }
+
+        if (moved) {
+            placeRandomTile();
+        }
+    }
+
+    public void moveRight() {
+        move(0, 1);
+    }
+
+    public void moveLeft() {
+        move(0, -1);
+    }
+
+    public void moveUp() {
+        move(-1, 0);
+    }
+
+    public void moveDown() {
+        move(1, 0);
     }
 
     public int getNumber(int i, int j) {
@@ -42,5 +97,26 @@ public class GridNumber {
         for (int[] line : numbers) {
             System.out.println(Arrays.toString(line));
         }
+    }
+
+    public static void main(String[] args) {
+        model.GridNumber grid = new model.GridNumber(4, 4);
+        grid.printNumber();
+
+        System.out.println("Moving Right");
+        grid.moveRight();
+        grid.printNumber();
+
+        System.out.println("Moving Down");
+        grid.moveDown();
+        grid.printNumber();
+
+        System.out.println("Moving Left");
+        grid.moveLeft();
+        grid.printNumber();
+
+        System.out.println("Moving Up");
+        grid.moveUp();
+        grid.printNumber();
     }
 }
